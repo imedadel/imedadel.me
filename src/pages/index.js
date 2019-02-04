d70712a26d15d5eb46f9dcbae240779aea55cec2
Imed Adel <aadelimed@gmail.com>
2019-02-04 16:12:07 +0100

    Add Meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,8 @@ export default () => (<main>
           name="viewport"
           content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
         />
+        <meta name="google-site-verification" content="zTgLUUdv7vAoD9OEXQf1Lurh9PxlHof08WW1h3p1cgw" />
+        <meta name="msvalidate.01" content="D2ECEF51E4DAF349AA4E295011F6A257" />
         
         <meta property="og:url" content="https://imedadel.me" />
         <meta property="og:type" content="website" />
