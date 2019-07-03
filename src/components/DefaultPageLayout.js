a549bece60f32fc51863fae4d9418ed8d25d08ca
Imed Adel <aadelimed@gmail.com>
2019-07-03 23:35:08 +0200

    restructure

--- src/components/DefaultPageLayout.js
+++ src/components/DefaultPageLayout.js
@@ -1,17 +1,40 @@
 import React from 'react'
-import { MDXRenderer } from "gatsby-mdx"
-import {Link} from 'gatsby'
+import { MDXRenderer } from 'gatsby-mdx'
+import { Link } from 'gatsby'
 
 import SEO from '../components/SEO'
 import { rhythm } from '../utils/typography'
 
-export default ({ pageContext: {node} }) => {
+import resume from '../assets/ImedAdelResume.pdf'
+
+export default ({ pageContext: { node } }) => {
 	// comment
 	return (
-	<main>
-		<SEO title={node.headings[0].value} description={node.frontmatter.desc} slug={node.frontmatter.slug} />
-		<Link to={`/`} style={{textDecoration: `none`, marginBottom: rhythm(1.5),}} title={`Go to homepage`} role={`navigation`}><h2 style={{ textTransform: 'lowercase' }}>Imed Adel.</h2></Link>
-		<MDXRenderer>{node.code.body}</MDXRenderer>
-	</main>
+		<>
+			<header>
+				<SEO
+					title={node.headings[0].value}
+					description={node.frontmatter.desc}
+					slug={node.frontmatter.slug}
+				/>
+				<Link
+					to={`/`}
+					style={{ textDecoration: `none`, marginBottom: rhythm(1.5) }}
+					title={`Go to homepage`}
+					role={`navigation`}
+				>
+					<h2 style={{ textTransform: 'lowercase' }}>Imed Adel.</h2>
+				</Link>
+			</header>
+			<main>
+				<MDXRenderer>{node.code.body}</MDXRenderer>
+			</main>
+			<footer>
+				<Link to={resume}>Resume</Link>,{' '}
+				<a href='https://www.linkedin.com/in/imedadel'>Linkedin</a>,{' '}
+				<a href='https://github.com/ImedAdel'>Github</a>,{' '}
+				<a href='https://twitter.com/Imed_Adel'>Twitter</a>.
+			</footer>
+		</>
 	)
 }
