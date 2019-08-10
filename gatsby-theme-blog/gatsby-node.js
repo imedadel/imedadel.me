f6dd478dc0fd548c305a4b9c18c8ee8634f88d16
Imed Adel <aadelimed@gmail.com>
2019-08-10 19:10:04 +0200

    set up page creation

---
+++ gatsby-theme-blog/gatsby-node.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+	if (node.internal.type !== 'Mdx') return
+
+	const value = path.join('/', createFilePath({ node, getNode }))
+	actions.createNodeField({
+		name: 'slug',
+		node,
+		value: value.replace(/\\+/g, ``).replace(/\/+/g, `/`).replace(/\/$/, ``),
+	})
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+	const { data } = await graphql(`
+		{
+			allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+				nodes {
+					id
+					timeToRead
+					frontmatter {
+						desc
+					}
+					fields {
+						slug
+					}
+					headings {
+						value
+					}
+					body
+				}
+			}
+		}
+	`)
+
+	data.allMdx.nodes.forEach(node => {
+		actions.createPage({
+			path: node.fields.slug,
+			component: require.resolve(`./src/templates/post.js`),
+			context: {
+				node,
+			},
+		})
+	})
+}
