9dd7298e900601ed57c496667a251c20f29d766b
Imed Adel <aadelimed@gmail.com>
2019-08-10 17:34:13 +0200

    add site metadata in config

---
+++ gatsby-theme-blog/gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	siteMetadata: {
+		title: 'Imed Adel\'s Blog',
+		description: 'The personal blog of Imed Adel',
+		author: 'Imed Adel',
+		siteUrl: 'http:s://imedadel.me',
+	},
+}
