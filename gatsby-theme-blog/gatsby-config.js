c0c71d3e9db9fe21b8b7f92f51073fcffcf51598
Imed Adel <aadelimed@gmail.com>
2019-08-12 15:35:40 +0200

    install prism

--- gatsby-theme-blog/gatsby-config.js
+++ gatsby-theme-blog/gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
 			resolve: 'gatsby-plugin-mdx',
 			options: {
 				extensions: ['.mdx', '.md'],
+				gatsbyRemarkPlugins: [
+					{
+						resolve: 'gatsby-remark-prismjs',
+						options: {
+							inlineCodeMarker: '÷',
+						},
+					},
+				],
 			},
 		},
 		{
