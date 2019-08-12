df1c7c4b78aab0ae35b6b1e2e682651266a16f12
Imed Adel <aadelimed@gmail.com>
2019-08-12 16:33:12 +0200

    define Seo component using React Helmet

---
+++ gatsby-theme-seo/src/components/Seo.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+const Seo = ({ title, desc, slug }) => (
+	<Helmet>
+		<meta charSet="utf-8" />
+		<title>{title ? `${title} — Imed Adel` : `Imed Adel`}</title>
+		<link rel="canonical" href="https://imedadel.me" />
+		<html lang="en" />
+		<meta
+			name="viewport"
+			content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+		/>
+		<meta
+			name="google-site-verification"
+			content="zTgLUUdv7vAoD9OEXQf1Lurh9PxlHof08WW1h3p1cgw"
+		/>
+		<meta name="msvalidate.01" content="D2ECEF51E4DAF349AA4E295011F6A257" />
+		<meta name="description" content={desc ? desc : `A blog by Imed Adel`} />
+
+		<meta
+			property="og:url"
+			content={`https://imedadel.me/${slug ? slug : ``}`}
+		/>
+		<meta property="og:type" content={slug ? `article` : `website`} />
+		<meta property="og:locale" content="en" />
+		<meta property="og:site_name" content="Imed Adel" />
+	</Helmet>
+)
+
+export default Seo
