b6c9671620c77c944bef535d79d372d23581161f
Imed Adel <aadelimed@gmail.com>
2019-08-13 15:28:32 +0200

    add google analytics

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-	plugins: [`gatsby-theme-blog`, `gatsby-theme-homepage`],
+	plugins: [
+		`gatsby-theme-blog`,
+		`gatsby-theme-homepage`,
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-133819882-3',
+			},
+		},
+	],
 }
