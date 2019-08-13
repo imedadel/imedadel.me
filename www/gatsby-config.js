d5956ec46bbe02dd45441006a31256ffa100e6d4
Imed Adel <aadelimed@gmail.com>
2019-08-13 15:34:50 +0200

    add web manifest

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -8,5 +8,19 @@ module.exports = {
 				trackingId: 'UA-133819882-3',
 			},
 		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: "Imed Adel",
+				short_name: "Imed Adel",
+				start_url: "/",
+				background_color: "#fff",
+				theme_color: "#000",
+				// Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+				// see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+				display: "standalone",
+				icon: "src/assets/icon.png", // This path is relative to the root of the site.
+			},
+		},
 	],
 }
